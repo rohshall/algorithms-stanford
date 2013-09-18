1e86da1e1bc9ef02bfc874898a2ed124b6d1525a
rmalyona <rmalyona@gmail.com>
2013-09-18 17:26:15 +0300

    Clustering Algorithm

---
+++ greedy/src/main/java/org/mcvly/algo/greedy/clustering/ClusteringAlgorithm.java
@@ -0,0 +1,11 @@
+package org.mcvly.algo.greedy.clustering;
+
+import org.mcvly.algo.greedy.mst.kruskal.UnionFindImplementation;
+
+/**
+ * @author <a href="mailto:RMalyona@luxoft.com">Ruslan Malyona</a>
+ * @since 18.09.13
+ */
+public class ClusteringAlgorithm extends UnionFindImplementation {
+
+}
